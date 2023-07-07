2f636919ba0e70d4f2ff9767f83f05cfebd1a3ff
席浩(29176184) <xihao004@ke.com>
2023-07-07 02:49:54 +0000

    update train

--- tutorial_train.py
+++ tutorial_train.py
@@ -1,4 +1,12 @@
-from share import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   tutorial_train.py
+@Time    :   2023/07/06 17:42:11
+@Author  :   xihao004
+@Version :   1.0
+@Desc    :   None
+'''
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
@@ -8,28 +16,23 @@ from cldm.model import create_model, load_state_dict
 
 
 # Configs
-resume_path = './models/control_sd15_ini.ckpt'
-batch_size = 4
-logger_freq = 300
-learning_rate = 1e-5
-sd_locked = True
-only_mid_control = False
-
-
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-model = create_model('./models/cldm_v15.yaml').cpu()
-model.load_state_dict(load_state_dict(resume_path, location='cpu'))
-model.learning_rate = learning_rate
-model.sd_locked = sd_locked
-model.only_mid_control = only_mid_control
-
-
-# Misc
-dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
-logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
-
-
-# Train!
-trainer.fit(model, dataloader)
+if __name__ == "__main__":
+    resume_path = './models/control_sd15_ini.ckpt'
+    batch_size = 4
+    logger_freq = 300
+    learning_rate = 1e-5
+    sd_locked = True
+    only_mid_control = False
+    # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+    model = create_model('./models/cldm_v15.yaml').cpu()
+    model.load_state_dict(load_state_dict(resume_path, location='cpu'))
+    model.learning_rate = learning_rate
+    model.sd_locked = sd_locked
+    model.only_mid_control = only_mid_control
+    # Misc
+    dataset = MyDataset()
+    dataloader = DataLoader(dataset, num_workers=8, batch_size=batch_size, shuffle=True)
+    logger = ImageLogger(batch_frequency=logger_freq)
+    trainer = pl.Trainer(gpus=2, precision=32, callbacks=[logger])
+    # Train!
+    trainer.fit(model, dataloader)
